8696afc21e407032571acb6b9a53dc53a539b246
Peter Barnum <peterbarnum@gmail.com>
2022-05-19 12:58:43 -0600

    add initial mockup/code for "getInvolved" view
    1. Amazon smile shop/donate card + link
    2. Donate to Operation Spark card with commitChange form (shows in modal)

--- pages/getInvolved/index.tsx
+++ pages/getInvolved/index.tsx
@@ -1,15 +1,249 @@
 import { NextPage } from 'next';
+import styled, { useTheme } from 'styled-components';
+import Image from 'next/image';
+import { useState } from 'react';
+import { IoMdCloseCircle } from 'react-icons/io';
+import { BsArrowRightSquareFill } from 'react-icons/bs';
+import { MdOpenInNew } from 'react-icons/md';
+
 import Content from '@this/components/layout/Content';
 import Main from '@this/components/layout/Main';
+import rgbDataURL from '@this/src/helpers/rgbDataURL';
+import Button from '@this/src/components/Elements/Button';
+import { cardShadow } from '@this/src/theme/styled/mixins/shadows';
 
 const GetInvolved: NextPage = () => {
+  const theme = useTheme();
+  const [showDonate, setShowDonate] = useState(false);
+
   return (
     <Main>
-      <Content>
-        <h1 className='dynamic-h1'>Get Involved</h1>
-      </Content>
+      <GetInvolvedStyles>
+        <Content>
+          <h1 className='dynamic-xl primary-secondary text-center'>
+            HELP OPEN THE DOOR TO PROSPERITY
+          </h1>
+
+          <div className='donation-methods'>
+            <div className='donate-box'>
+              <a
+                href='https://smile.amazon.com/ch/47-1514606'
+                target='_blank'
+                rel='noreferrer'
+                title='Amazon Smile'
+                className='donate-wrapper'
+              >
+                <p className='dynamic-txt'>
+                  Shop at Amazon Smile and 0.5% of eligible purchases will be
+                  donated to Operation Spark!
+                </p>
+
+                <div className='img'>
+                  <Image
+                    src={`/images/logos/etc/amazon-smile-halle-${theme.colorMode}.png`}
+                    layout='fill'
+                    objectFit='contain'
+                    alt='Amazon Smile | Operation Spark'
+                    quality={100}
+                    blurDataURL={rgbDataURL(134, 0, 241)}
+                    loading='eager'
+                    priority
+                  />
+                </div>
+
+                <button className='anchor'>
+                  Shop Now!{' '}
+                  <span className='icon'>
+                    <MdOpenInNew />
+                  </span>
+                </button>
+              </a>
+            </div>
+            <div className='donate-box'>
+              <div className='donate-wrapper' title='Donate to Operation Spark'>
+                <p className='dynamic-txt desc'>
+                  We depend on dedicated individuals and organizations to run
+                  our programs.
+                </p>
+                <p className='dynamic-txt desc'>
+                  Your donation can make the following possible:
+                </p>
+                <div className='donation-list'>
+                  {[
+                    [25, 'Our hosting bill for a month'],
+                    [250, 'Chromebook for a student'],
+                    [825, 'Bootcamp for one student'],
+                  ].map(([amt, desc], i) => (
+                    <p
+                      className='dynamic-txt primary-secondary li'
+                      key={`${amt}-${desc}-${i}`}
+                    >
+                      <div className='amount'>${amt}</div>
+                      <BsArrowRightSquareFill className='icon' size={18} />{' '}
+                      {desc}
+                    </p>
+                  ))}
+                </div>
+
+                <div className='img'>
+                  <Image
+                    src={`/images/hallebot3d.png`}
+                    layout='fill'
+                    objectFit='contain'
+                    alt='Amazon Smile | Operation Spark'
+                    quality={100}
+                    blurDataURL={rgbDataURL(134, 0, 241)}
+                    loading='eager'
+                    priority
+                  />
+                </div>
+                <Button
+                  color='yellow'
+                  onClick={() => setShowDonate(!showDonate)}
+                >
+                  Donate to Operation Spark!
+                </Button>
+                <p className='tax-deduct'>
+                  <span style={{ userSelect: 'none' }}>
+                    Your donations are tax deductible. Our EIN is{' '}
+                  </span>
+                  47-1514606
+                </p>
+              </div>
+            </div>
+          </div>
+          {showDonate && (
+            <div className='donate-modal' onClick={() => setShowDonate(false)}>
+              <div
+                className='iframe-wrapper'
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div className='overlay-btn'>
+                  <Button onClick={() => setShowDonate(false)}>
+                    <IoMdCloseCircle size={30} />
+                  </Button>
+                </div>
+                <iframe
+                  src='https://commitchange.com/nonprofits/3745/donate?offsite=t&amp;origin=http://operationspark.org/#!/donate'
+                  width='400px'
+                  height='500px'
+                ></iframe>
+              </div>
+            </div>
+          )}
+        </Content>
+      </GetInvolvedStyles>
     </Main>
   );
 };
 
 export default GetInvolved;
+
+const GetInvolvedStyles = styled.div`
+  .donate-modal {
+    position: fixed;
+    z-index: 1000;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: ${({ theme }) => theme.alpha.bg};
+    backdrop-filter: blur(8px);
+    .iframe-wrapper {
+      margin: 1rem;
+      position: relative;
+      .overlay-btn {
+        position: absolute;
+        z-index: 1;
+        top: 1rem;
+        right: 0.25rem;
+        button {
+          background: ${({ theme }) =>
+            theme.isLightMode ? 'rgba(255,255,255,1)' : 'rgba(29, 29, 29, 1)'};
+          color: ${({ theme }) => theme.fg};
+          :hover {
+            box-shadow: none;
+            color: ${({ theme }) => theme.red[300]};
+          }
+        }
+      }
+      iframe {
+        user-select: none;
+        max-width: 100%;
+        filter: ${({ theme }) =>
+          theme.isLightMode ? 'invert(0)' : 'invert(0.9) grayscale(100%)'};
+      }
+    }
+  }
+  .donation-methods {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+    align-items: stretch;
+    padding: 2rem 0;
+  }
+  .donate-box {
+    width: 400px;
+    max-width: 100%;
+
+    border-radius: 0.25rem;
+    ${cardShadow};
+
+    margin: 1rem;
+    .donate-wrapper {
+      display: flex;
+      flex-flow: column;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+      padding: 1rem 2rem;
+      p.desc {
+        padding: 0.25rem 0;
+      }
+      .tax-deduct {
+        color: ${({ theme }) => theme.grey[500]};
+        font-size: 0.8rem;
+        padding-top: 0.5rem;
+      }
+      button.anchor {
+        display: flex;
+        align-items: center;
+        .icon {
+          margin-left: 0.5rem;
+        }
+      }
+    }
+    .img {
+      * {
+        -webkit-user-drag: none;
+        user-select: none;
+      }
+      overflow: hidden;
+      position: relative;
+      aspect-ratio: 3 / 1;
+      width: 500px;
+      max-width: 100%;
+      height: 100%;
+      border-radius: 0.5rem;
+      transition: box-shadow 125ms;
+    }
+  }
+  .donation-list {
+    width: 100%;
+    padding: 0.5rem 0;
+    p.li {
+      width: 100%;
+      padding: 0.25rem 0;
+      display: flex;
+      align-items: center;
+      .amount {
+        width: 50px;
+      }
+      .icon {
+        margin-right: 0.5rem;
+      }
+    }
+  }
+`;
