f4aff107f5c9ea9d77b0d29ab545861d35e5fed2
Peter Barnum <peterbarnum@gmail.com>
2022-06-11 03:33:20 -0600

    background texture?

--- src/theme/styled/GlobalStyles.ts
+++ src/theme/styled/GlobalStyles.ts
@@ -5,6 +5,9 @@ const GlobalStyles = createGlobalStyle`
   * {
     transition: background-color 125ms;
   }
+  body {
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23888888' fill-opacity='0.2' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E") !important;
+  }
   .primary-secondary {
     color: ${({ theme }) => (theme.isLightMode ? theme.primary[700] : theme.secondary[500])};
   }
