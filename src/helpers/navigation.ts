377bb52aedbd82a3f36126f0135b2b3f724f157f
Peter Barnum <peterbarnum@gmail.com>
2022-08-01 09:33:59 -0600

    verboseness for the win!

--- src/helpers/navigation.ts
+++ src/helpers/navigation.ts
@@ -1,3 +1,12 @@
 export const checkActiveSubLink = (href1: string, href2: string): boolean => {
-  return href1 === href2 ? true : href1.split('/').pop() === href2.split('/').pop();
+  if (href1 === href2) {
+    return true;
+  }
+  const hrefArr1 = href1.split('/');
+  const hrefArr2 = href2.split('/');
+
+  const lastPath1 = hrefArr1.pop();
+  const lastPath2 = hrefArr2.pop();
+
+  return !!lastPath1 && lastPath1 === lastPath2;
 };
