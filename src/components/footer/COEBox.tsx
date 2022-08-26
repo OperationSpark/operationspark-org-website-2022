342c6464a1b9cf2419d64de97bf7e9181593fd06
Peter Barnum <peterbarnum@gmail.com>
2022-08-25 22:27:26 -0600

    move accreditation disclaimer to footer

---
+++ src/components/footer/COEBox.tsx
@@ -0,0 +1,188 @@
+import styled from 'styled-components';
+import { AnimatePresence, motion } from 'framer-motion';
+import { cardShadow } from '@this/src/theme/styled/mixins/shadows';
+import { IoMdCloseCircle as CloseIcon } from 'react-icons/io';
+import { AiOutlineInfoCircle as InfoIcon } from 'react-icons/ai';
+
+import Content from '@this/components/layout/Content';
+import { useClickAway } from '@this/src/hooks/useClickAway';
+
+const COEBox = () => {
+  const [coeInfoRef, showCoeInfo, setShowCoeInfo] = useClickAway();
+
+  return (
+    <COEBoxStyles>
+      <Content className='coe-box-content'>
+        <div className='overflow-box'>
+          <div className='overflow-content'>
+            <p className='dynamic-txt coe-header blurry-box'>
+              Operation Spark is applying to become a candidate for accreditation with the
+              Commission of the Council on Occupational Education.{' '}
+              <button className='anchor' onClick={() => setShowCoeInfo(!showCoeInfo)}>
+                Learn more
+              </button>
+              <button className='info-button' onClick={() => setShowCoeInfo(!showCoeInfo)}>
+                <InfoIcon size={18} />
+              </button>
+            </p>
+            <AnimatePresence>
+              {showCoeInfo && (
+                <motion.div
+                  className='overflow-info-box'
+                  initial={{ height: 0, opacity: 0 }}
+                  animate={{ height: 'auto', opacity: 1 }}
+                  exit={{ height: 0, opacity: 0 }}
+                  transition={{ type: 'tween', duration: 0.1 }}
+                >
+                  <div className='overflow-info-box-content blurry-box' ref={coeInfoRef}>
+                    <button className='info-button close' onClick={() => setShowCoeInfo(false)}>
+                      <CloseIcon size={20} />
+                    </button>
+                    <p className='dynamic-txt primary-secondary'>
+                      Operation Spark is applying to become a candidate for accreditation with the
+                      Commission of the Council on Occupational Education.{' '}
+                    </p>
+                    <p className='dynamic-txt' style={{ marginTop: '1rem' }}>
+                      <b>Anyone wishing to make comments should either:</b>
+                    </p>
+                    <ul className='dynamic-txt' style={{ paddingLeft: '1rem' }}>
+                      <li>
+                        Write to the Executive Director of the Commission:
+                        <div
+                          style={{
+                            userSelect: 'all',
+                            fontSize: '1rem',
+                            paddingLeft: '1rem',
+                          }}
+                        >
+                          <b>
+                            Council on Occupational Education <br />
+                            7840 Roswell Road
+                            <br />
+                            Building 300, Suite 325
+                            <br />
+                            Atlanta, Georgia 30350
+                            <br />
+                          </b>
+                        </div>
+                      </li>
+
+                      <li>
+                        Submit comments through the Council’s website{' '}
+                        <a
+                          className='anchor'
+                          href='https://council.org/'
+                          target='_blank'
+                          rel='noreferrer'
+                        >
+                          council.org
+                        </a>
+                      </li>
+                    </ul>
+                    <p className='dynamic-txt'>
+                      <b>Anyone making comments must provide their name and mailing address.</b>
+                    </p>
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+        </div>
+      </Content>
+    </COEBoxStyles>
+  );
+};
+
+export default COEBox;
+
+export const COEBoxStyles = styled.div`
+
+  .coe-box-content {
+    font-family: 'Ubuntu', sans-serif;
+    position: relative;
+    z-index: 2;
+    width: 100%;
+    display: flex;
+    flex-flow: column;
+    justify-content: flex-end;
+
+    h1 {
+      line-height: 1.25em;
+    }
+
+    h2 {
+      padding-bottom: 1rem;
+    }
+    .blurry-box {
+      ${cardShadow};
+      padding: 0.75rem 0.5rem;
+      backdrop-filter: blur(8px);
+      border-radius: 0.25rem;
+    }
+  }
+  .overflow-box {
+    position: relative;
+    padding: 1rem 0;
+    margin-bottom: 3rem;
+    .info-button {
+      position: absolute;
+      top: 0;
+      right: 0;
+      padding: 0.25rem;
+      color: ${({ theme }) => theme.magenta[200]};
+      :hover {
+        color: ${({ theme }) => theme.magenta[400]};
+      }
+    }
+    .info-button.close {
+      color: ${({ theme }) => theme.red[300]};
+      :hover {
+        color: ${({ theme }) => theme.red[400]};
+      }
+    }
+    .overflow-content {
+      position: absolute;
+      width: 100%;
+      .coe-header {
+        background: ${({ theme }) => theme.alpha.bg};
+        width: calc(400px + 15vw);
+        max-width: 100%;
+        margin: 0 auto;
+        padding: 0.5rem 0.5rem;
+        padding-right: 1.5rem;
+      }
+      .overflow-info-box {
+        position: absolute;
+        width: 100%;
+        top: 0;
+        display: flex;
+        justify-content: center;
+        .overflow-info-box-content {
+          overflow: hidden;
+          li {
+            padding-bottom: 0.5rem;
+          }
+          max-width: 100%;
+          width: calc(400px + 15vw);
+          background: ${({ theme }) => theme.alpha.bg};
+          padding: 0.5rem;
+          padding-right: 1.5rem;
+        }
+      }
+    }
+  }
+  margin-bottom: 3rem;
+  @media screen and (max-width: 768px) {
+    .top-card-content {
+      .college-credit {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        .college-credit-box {
+          background: rgba(0, 0, 0, 0);
+          width: 100%;
+        }
+      }
+    }
+  }
+`;
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,15 +6,15 @@ import { useRouter } from 'next/router';
 import { VStack, HStack, Text } from '@chakra-ui/react';
 import { useTheme } from 'styled-components';
 
-import Subscribe from './subscribe';
 import { SlashDivider } from '@this/components/Elements/SlashDivider';
 import NavLink from '@this/components/Navbar/elements/NavLink';
-
-import { InfoSessionStyles, FooterStyles, SupportersStyles } from './FooterStyles';
-
 import { ILogo } from '@this/data/types/logos';
 import rgbDataURL from '@this/src/helpers/rgbDataURL';
 import SocialNetworks from '@this/components/Elements/SocialNetworks';
+
+import { InfoSessionStyles, FooterStyles, SupportersStyles } from './FooterStyles';
+import Subscribe from './subscribe';
+import COEBox from './COEBox';
 
 interface FooterProps {
   logos: ILogo[];
@@ -44,7 +44,7 @@ const Footer = ({ logos }: FooterProps) => {
       <SlashDivider />
 
       <Subscribe />
-
+      <COEBox />
       <VStack w='100%' className='content' pb='0'>
         <SupportersStyles w='100%'>
           <VStack justify='space-between' borderColor='brand.purple.900' borderBottom='1px'>
