58bc772cf79700d488b97ac6905f9e2a8d9e9ac6
Peter Barnum <peterbarnum@gmail.com>
2022-09-09 16:09:32 -0600

    init asterisk disclaimer component

--- src/components/Home/IgniteCareer.tsx
+++ src/components/Home/IgniteCareer.tsx
@@ -1,21 +1,26 @@
 import styled from 'styled-components';
 import { IHome } from '@this/data/types/home';
 import Content from '@this/components/layout/Content';
+import Asterisk from '../Elements/Asterisk';
 
 const IgniteCareer = ({ title, description }: IHome['igniteCareer']) => {
   return (
     <IgniteCareerStyles>
       <Content>
         {title.map((text) => (
-          <h1 key={text} className='dynamic-h1'>
+          <h1 key={text} className='dynamic-h1 primary-secondary'>
             {text}
           </h1>
         ))}
-        {description.map((text) => (
-          <h2 key={text} className='dynamic-h3'>
-            {description}
-          </h2>
-        ))}
+
+        <h2 className='dynamic-h3'>
+          <Asterisk
+            matcher='6 months!'
+            infoMessage='6 months of course time. Actual program length may very depending on length between phases'
+          >
+            {description.join(' ')}
+          </Asterisk>
+        </h2>
       </Content>
     </IgniteCareerStyles>
   );
@@ -26,13 +31,12 @@ export default IgniteCareer;
 const IgniteCareerStyles = styled.div`
   padding: 2rem 0;
   h1 {
-    color: ${({ theme }) => (theme.isLightMode ? theme.primary[0] : theme.secondary[0])};
     margin-bottom: 2rem;
   }
   h2 {
     color: ${({ theme }) => theme.fg};
     font-weight: 700;
-    width: 80%;
+    width: 100%;
   }
 
   @media screen and (max-width: 768px) {
