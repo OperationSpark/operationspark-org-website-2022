0a2ca4dd2394a8329dc45f6ea10538be3b3bd57e
Peter Barnum <peterbarnum@gmail.com>
2022-11-10 20:06:11 -0700

    add test bar to top of page for `test` environment

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from 'react';
 import styled, { useTheme } from 'styled-components';
 import moment from 'moment-timezone';
 import { motion, Transition } from 'framer-motion';
+import { HiOutlineBeaker as TestIcon } from 'react-icons/hi';
 
 import { navMenus } from './navLinks';
 import { IAlert } from '@this/data/types/bits';
@@ -15,6 +16,8 @@ const ProgressBar = dynamic(() => import('./ProgressBar'));
 const AlertBar = dynamic(() => import('./AlertBar'));
 const DesktopNav = dynamic(() => import('./DesktopNav'));
 const MobileNav = dynamic(() => import('./MobileNav'));
+
+const { NODE_ENV } = process.env;
 
 interface NavProps {
   alertInfo: IAlert;
@@ -90,7 +93,13 @@ export default function Nav({ alertInfo }: NavProps) {
 
   return (
     <NavbarStyles ref={navRef} animate={navAnimation} transition={navTransition}>
+      {NODE_ENV !== 'test' ? null : (
+        <div className='test-mode'>
+          <TestIcon /> &nbsp;TEST MODE&nbsp; <TestIcon />
+        </div>
+      )}
       {showAlert && <AlertBar info={alertInfo} />}
+
       <div className='navbar'>
         <LogoLink
           className='nav-logo-desktop'
@@ -129,7 +138,29 @@ export const NavbarStyles = styled(motion.nav)`
   justify-content: space-between;
   align-items: center;
   flex-flow: row wrap;
-
+  .test-mode {
+    display: flex;
+    width: 100%;
+    background: ${({ theme }) => `
+    linear-gradient(0deg,
+      ${theme.red[800]} -25%,
+      ${theme.magenta[700]} 35%,
+      ${theme.magenta[700]} 65%,
+      ${theme.red[800]} 125%
+    )
+    `};
+    color: ${({ theme }) => theme.white};
+    z-index: 1000;
+    font-weight: 700;
+    line-height: 1.25em;
+    font-family: 'Source Code Pro', monospace;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    svg {
+      color: ${({ theme }) => theme.green[500]};
+    }
+  }
   .navbar {
     display: flex;
     height: fit-content;
