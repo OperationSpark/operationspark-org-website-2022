ef2a10f0dcd97cf49cd2ab40d6379f680bb3f7a8
Peter Barnum <peterbarnum@gmail.com>
2022-12-12 15:26:01 -0700

    Add `type AttendingLocation` type for `"IN_PERSON"` or `"VIRTUAL"`

--- data/types/infoSession.d.ts
+++ data/types/infoSession.d.ts
@@ -5,6 +5,8 @@ export type CommonQuestions = {
   answer: string[];
 };
 
+type AttendingLocation = 'IN_PERSON' | 'VIRTUAL';
+
 export interface IInfoSession {
   commonQuestions: CommonQuestions[];
   infoSessionForm: {
@@ -28,5 +30,5 @@ export interface IInfoSessionFormValues {
   referencedBy: SelectItem;
   sessionDate: SelectItem;
   userLocation: SelectItem;
-  attendingLocation: string;
+  attendingLocation: AttendingLocation;
 }
