8f4860addb87206bff969ae65f2231e5436c13f2
Peter Barnum <peterbarnum@gmail.com>
2023-03-20 12:15:54 -0600

    Fix/hs form (#73)
    * update "spring" → "summer"
    
    * remove "summer"

--- src/components/Elements/AtlantaPromo.tsx
+++ src/components/Elements/AtlantaPromo.tsx
@@ -143,15 +143,17 @@ const AtlantaPromoStyles = styled(motion.div)`
       pointer-events: none;
     }
 
-    .open-btn > button {
-      transition: all 250ms;
-      color: ${({ theme }) => theme.secondary[700]};
-    }
-    .open-btn > button {
-      :hover {
+    .open-btn {
+      button > .expand-icon {
         transition: all 250ms;
-        color: ${({ theme }) => theme.green[0]};
-        transform: rotate(-90deg);
+        color: ${({ theme }) => theme.secondary[700]};
+      }
+      :hover {
+        button > .expand-icon {
+          transition: all 250ms;
+          color: ${({ theme }) => theme.green[0]};
+          transform: rotate(-90deg);
+        }
       }
     }
   }
@@ -168,10 +170,17 @@ const AtlantaPromoStyles = styled(motion.div)`
       right: 1rem;
       top: 1.25rem;
     }
+    :hover {
+      button > .close-icon {
+        color: ${({ theme }) => theme.red[0]};
+        transform: rotate(90deg);
+      }
+    }
     button > .close-icon {
       border-radius: 50%;
       transition: all 250ms;
       padding: 0;
+      pointer-events: none;
       :hover {
         color: ${({ theme }) => theme.red[0]};
         transform: rotate(90deg);
@@ -205,18 +214,20 @@ const AtlantaPromoStyles = styled(motion.div)`
 
     .promo-anchor {
       padding: 1rem 0;
+      padding-bottom: 0;
       transition: all 250ms;
       font-size: 1.5rem;
       color: ${({ theme }) => theme.secondary[400]};
       text-shadow: 0 0 0.25rem ${({ theme }) => theme.black};
       filter: drop-shadow(0 0 0.4rem ${({ theme }) => theme.secondary[900]});
       box-shadow: 0rem 0rem 0rem ${({ theme }) => theme.secondary[900]},
-        0rem 0rem 0rem ${({ theme }) => theme.secondary[800]};
+        0rem 0rem 0rem ${({ theme }) => theme.secondary[800]},
+        0rem 0rem 0rem ${({ theme }) => theme.black} inset;
       line-height: 1.25em;
       margin-top: 1.5rem;
       font-weight: 900;
       letter-spacing: 1.25px;
-      border-radius: 2rem 2rem 3rem 3rem;
+      border-radius: 2rem;
       backdrop-filter: blur(3px);
       .learn-more-text {
         font-size: 0.8rem;
@@ -225,17 +236,17 @@ const AtlantaPromoStyles = styled(motion.div)`
       }
 
       :hover {
-        backdrop-filter: blur(5px);
         filter: drop-shadow(0 0 0.5rem ${({ theme }) => theme.secondary[200]});
         box-shadow: 0.1rem 0.1rem 0.2rem ${({ theme }) => theme.secondary[900]},
-          -0.1rem -0.1rem 0.2rem ${({ theme }) => theme.secondary[800]};
+          -0.1rem -0.1rem 0.2rem ${({ theme }) => theme.secondary[800]},
+          0rem 0rem 0rem ${({ theme }) => theme.black} inset;
       }
       :active {
-        backdrop-filter: blur(5px);
         transition: all 125ms;
         filter: drop-shadow(0 0 1rem ${({ theme }) => theme.secondary[900]});
         box-shadow: 0rem 0rem 0.1rem ${({ theme }) => theme.secondary[900]},
-          0rem 0rem 0.1rem ${({ theme }) => theme.secondary[800]};
+          0rem 0rem 0.1rem ${({ theme }) => theme.secondary[800]},
+          0rem 0rem 0.5rem ${({ theme }) => theme.black} inset;
       }
     }
 
