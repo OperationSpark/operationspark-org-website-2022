583dbae271ff257ac69de0a49db1a80903cae1f1
Peter Barnum <peterbarnum@gmail.com>
2024-03-28 18:12:58 -0600

    move referenceOptions to new file

--- src/Forms/Form.Workforce.tsx
+++ src/Forms/Form.Workforce.tsx
@@ -13,6 +13,7 @@ import { FormDataSignup } from '@this/pages-api/infoSession/user';
 import Spinner from '../components/Elements/Spinner';
 import { getStateFromZipCode } from '../components/Form/helpers';
 import useKeyCombo from '../hooks/useKeyCombo';
+import { referencedByOptions } from './formData/referenceOptions';
 
 interface WorkforceFormProps {
   sessionDates: ISessionDates[];
@@ -443,56 +444,5 @@ const workforceFormInputs = [
     name: 'phone',
     placeholder: '303-123-9876',
     required: true,
-  },
-];
-
-const referencedByOptions = [
-  {
-    value: 'google',
-    name: 'Google',
-  },
-  {
-    value: 'facebook',
-    name: 'Facebook',
-  },
-  {
-    value: 'instagram',
-    name: 'Instagram',
-  },
-  {
-    value: 'flyer',
-    name: 'Flyer',
-  },
-  {
-    value: 'radio',
-    name: 'Radio Advertising',
-  },
-  {
-    value: 'tv-streaming',
-    name: 'T.V. or Streaming Service',
-  },
-  {
-    value: 'snap',
-    name: 'SNAP',
-  },
-  {
-    value: 'other-advertising',
-    name: 'Other Advertising',
-    additionalInfo: 'Where did you hear about us?',
-  },
-  {
-    value: 'verbal',
-    name: 'Word of mouth',
-    additionalInfo: 'Who told you about us?',
-  },
-  {
-    value: 'event',
-    name: 'Community Event',
-    additionalInfo: 'Which event?',
-  },
-  {
-    value: 'organization',
-    name: 'Community Organization',
-    additionalInfo: 'Which organization?',
   },
 ];
