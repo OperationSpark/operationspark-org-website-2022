abd08e3a6760d6373149f3ba3d6831e8a92293a6
Peter Barnum <peterbarnum@gmail.com>
2024-05-01 12:12:29 -0600

    remove check inside hook -- logic moved to server

--- src/hooks/useShowcase.ts
+++ src/hooks/useShowcase.ts
@@ -1,25 +1,17 @@
 import axios, { isAxiosError } from 'axios';
 
 import { Showcase } from '@this/data/types/gradShowcase';
-import { toDayJs } from '@this/src/helpers/time';
 import { useEffect, useState } from 'react';
 
 const getShowcase = async () => {
   try {
-    const { data } = await axios.get<Showcase>('/api/showcase');
+    const { data: showcase } = await axios.get<Showcase>('/api/showcase');
 
-    if (!data || !data.startDateTime || !data.websiteActive || !data.active) {
+    if (!showcase) {
       return null;
     }
 
-    const disableTime = toDayJs(data.startDateTime).add(data.doorsOffset ?? 30, 'minutes');
-
-    if (disableTime.isBefore(toDayJs())) {
-      console.info('Showcase inactive');
-      return null;
-    }
-
-    return data;
+    return showcase;
   } catch (err) {
     if (isAxiosError(err) && err.response?.status === 404) {
       console.info('Showcase inactive');
