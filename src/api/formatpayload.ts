2e5423d9cd0f118fe88e330efcffdbb96d37e4af
Peter Barnum <peterbarnum@gmail.com>
2024-09-24 14:22:36 -0600

    move `formatPayload` to new file

---
+++ src/api/formatpayload.ts
@@ -0,0 +1,43 @@
+import config from '@this/config';
+import { FormDataSignup, ISessionSignup } from '@this/types/signups';
+
+export const formatSignupPayload = (form: FormDataSignup): ISessionSignup => {
+  const {
+    session,
+    email,
+    firstName,
+    lastName,
+    phone,
+    referencedBy,
+    zipCode,
+    userLocation: location,
+    attendingLocation,
+    smsOptIn,
+  } = form;
+
+  // Sometimes greenlight returns an empty string rather than a google place object
+  if (typeof session?.googlePlace === 'string') {
+    session.googlePlace = undefined;
+  }
+
+  return {
+    nameFirst: firstName,
+    nameLast: lastName,
+    email,
+    cell: phone,
+    sessionId: session?.id ?? '',
+    programId: session?.programId ?? '',
+    startDateTime: session?.startDateTime,
+    cohort: session?.cohort ?? '',
+    referrerResponse: referencedBy.additionalInfo,
+    referrer: referencedBy?.value,
+    locationType: session?.locationType,
+    googlePlace: session?.googlePlace,
+    token: config.GREENLIGHT_API_TOKEN,
+    smsOptIn: smsOptIn === 'true',
+    zipCode,
+    userLocation: location.additionalInfo || location.value,
+    attendingLocation,
+    joinCode: session?.code,
+  };
+};
