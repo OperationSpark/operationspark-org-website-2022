d973c1fa82b3d65fa99adaafe363d915bb1a414c
Peter Barnum <peterbarnum@gmail.com>
2025-02-03 17:23:44 -0700

    add default className to content component

--- src/components/layout/Content.tsx
+++ src/components/layout/Content.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-export const Content = styled.div`
+export const Content = styled.div.attrs(({ className }) => ({
+  className: ['__content-container', className].filter(Boolean).join(' '),
+}))`
   max-width: calc(4vw + 1200px);
   width: 100%;
   margin: 0 auto;
