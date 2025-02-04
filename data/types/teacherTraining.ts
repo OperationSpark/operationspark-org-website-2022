5148d4d5db421d68ef1738f61a1e389e5a1eadc0
Peter Barnum <peterbarnum@gmail.com>
2025-02-04 16:01:43 -0700

    add teacher training info page data

--- data/types/teacherTraining.ts
+++ data/types/teacherTraining.ts
@@ -1,3 +1,45 @@
+export type TeacherTrainingInfo = {
+  level: string;
+  title: string;
+  summary: string;
+  format: string;
+  times: {
+    startDate: string;
+    endDate: string;
+    startTime: string;
+    endTime: string;
+    examDay: string;
+    days: string;
+  };
+  cost: {
+    amount: string;
+    /** per: "Person" | per: "Group"  */
+    per: string;
+    includeTesting: boolean;
+  };
+
+  prerequisites: string;
+  outcomes: {
+    title?: string;
+    availabilityScore?: string;
+    description: string[];
+  }[];
+
+  nda: {
+    title: string;
+    description: string;
+  };
+
+  registration: {
+    priorityDate: string;
+    deadline: string;
+    refundDeadline: string;
+  };
+};
+
 export type TeacherTraining = {
   partners: string[];
+  info: {
+    [level: string]: TeacherTrainingInfo;
+  };
 };
