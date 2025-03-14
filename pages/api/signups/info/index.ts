2840639dbde2d7883fe4caafff06e850d97e321e
Harvey Sanders <sanders.harvey@gmail.com>
2025-03-14 14:59:05 -0400

    add Sentry header extraction

--- pages/api/signups/info/index.ts
+++ pages/api/signups/info/index.ts
@@ -1,6 +1,7 @@
 import { runCloudFunction } from '@this/api/googleFunctions';
 import config from '@this/config';
 import { formatSignupPayload } from '@this/src/api/formatSignupPayload';
+import { extractSentryHeadersFromRequest } from '@this/src/api/sentry';
 import { ReqHandler } from '@this/types/request';
 import { FormDataSignup, ISessionSignup } from '@this/types/signups';
 
@@ -17,23 +18,15 @@ type SignupResult = { url: string };
 const handleInfoSessionFormSubmit: ReqHandler<{}, FormDataSignup> = async (req: Req, res: Res) => {
   try {
     const payload = formatSignupPayload(req.body);
+    const sentryHeaders = extractSentryHeadersFromRequest(req);
 
-    const headers: {
-      [key: string]: string;
-    } = {
-      'X-Greenlight-Signup-Api-Key': GREENLIGHT_API_TOKEN,
-    };
-
-    if (typeof req.headers.baggage === 'string') {
-      headers.baggage = req.headers.baggage;
-    }
-    if (typeof req.headers['sentry-trace'] === 'string') {
-      headers['sentry-trace'] = req.headers['sentry-trace'];
-    }
     const result = await runCloudFunction<ISessionSignup, SignupResult>({
       url: SIGNUP_API_ENDPOINT,
       body: payload,
-      headers,
+      headers: {
+        'X-Greenlight-Signup-Api-Key': GREENLIGHT_API_TOKEN,
+        ...sentryHeaders,
+      },
     });
 
     res.status(200).json(result.data);
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,10 +2,10 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://2b3b08a6edc118c629a1e68874df48ae@o4508836956078080.ingest.us.sentry.io/4508966651494400",
+  dsn: 'https://2b3b08a6edc118c629a1e68874df48ae@o4508836956078080.ingest.us.sentry.io/4508966651494400',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
