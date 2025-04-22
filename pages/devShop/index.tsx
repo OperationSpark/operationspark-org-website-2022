1fdfbdb3bcf2fa1f76929d22b72cde1e692d47dc
Peter Barnum <peterbarnum@gmail.com>
2025-04-22 17:09:23 -0600

    add initial form modal

--- pages/devShop/index.tsx
+++ pages/devShop/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps, NextPage } from 'next';
+import { useState } from 'react';
 import styled from 'styled-components';
 
 import Content from '@this/components/layout/Content';
@@ -11,14 +12,25 @@ import GridList from '@this/src/components/Elements/GridList';
 import Markdown from '@this/src/components/layout/Markdown';
 
 const DevShop: NextPage<DevShopType> = ({ title, subtitle, about, skills, contractToHire }) => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <Main style={{ paddingTop: 0 }}>
       <DevShopStyles>
+        <button className='req-info-btn' onClick={() => setShowModal(true)}>
+          Request Info
+        </button>
+        {showModal && (
+          <div className='modal-container'>
+            <div className='modal-content'>
+              <h1 className='dynamic-h2'>TODO: Request Info Modal</h1>
+            </div>
+          </div>
+        )}
         <BgImg src='/images/display/abstract-space.webp' height='18rem'>
           <Content className='page-header-container'>
             <div className='page-header-content'>
               <h1 className='dynamic-xl secondary'>{title}</h1>
-              <h2 className='dynamic-h2'>{subtitle}</h2>
+              <h2 className='dynamic-h2 fw-800'>{subtitle}</h2>
             </div>
           </Content>
         </BgImg>
@@ -26,7 +38,7 @@ const DevShop: NextPage<DevShopType> = ({ title, subtitle, about, skills, contra
         <Content>
           <NeumorphismListCard
             title='Who We Are'
-            width='900px'
+            width='1000px'
             color='primary'
             style={{ margin: '0 auto', backdropFilter: 'blur(0.5rem)' }}
             items={[
@@ -39,7 +51,7 @@ const DevShop: NextPage<DevShopType> = ({ title, subtitle, about, skills, contra
         <Content>
           <NeumorphismListCard
             title={skills.title}
-            width='900px'
+            width='1000px'
             color='secondary'
             center
             style={{ margin: '0 auto' }}
@@ -53,9 +65,9 @@ const DevShop: NextPage<DevShopType> = ({ title, subtitle, about, skills, contra
                   className={`dynamic-txt sub-card${grid ? ' text-center' : ''}`}
                   key={`skills-performance-1-${i}-${header}`}
                 >
-                  {text}
+                  <Markdown>{text}</Markdown>
                 </p>
-                {grid && <GridList items={grid} itemStyle={{ flex: '1 1 150px' }} />}
+                {grid && <GridList items={grid} itemStyle={{ flex: '1 1 fit-content' }} />}
               </div>
             ))}
           />
@@ -65,7 +77,7 @@ const DevShop: NextPage<DevShopType> = ({ title, subtitle, about, skills, contra
             title='How Can Operation Spark Help My Company'
             subtitle='Contract to Hire or Our Dev Shop'
             color='primary'
-            width='900px'
+            width='1000px'
             style={{ margin: '0 auto' }}
             center
             items={contractToHire.sections.map((section, i) => (
@@ -95,7 +107,10 @@ const DevShop: NextPage<DevShopType> = ({ title, subtitle, about, skills, contra
                   />
                 )}
                 {section.tagline && (
-                  <div className='dynamic-txt text-center' style={{ marginTop: '1.5rem' }}>
+                  <div
+                    className='dynamic-txt text-center primary-secondary'
+                    style={{ marginTop: '1.5rem' }}
+                  >
                     <Markdown>{section.tagline}</Markdown>
                   </div>
                 )}
@@ -118,6 +133,7 @@ export const getStaticProps: GetStaticProps<DevShopType> = async () => {
 const DevShopStyles = styled.div`
   position: relative;
   z-index: 0;
+  margin-bottom: 4rem;
   &::after {
     position: fixed;
     content: '';
@@ -159,5 +175,66 @@ const DevShopStyles = styled.div`
       color: ${({ theme }) =>
         theme.isLightMode ? theme.rgb('green.900', 1) : theme.rgb('green.500', 1)};
     }
+  }
+  .req-info-btn {
+    position: fixed;
+    top: ${({ theme }) => `${theme.navHeight - 20}px`};
+    right: 1rem;
+    z-index: 1000;
+    padding: 0.5rem 1rem;
+    border-radius: 0.75rem;
+    font-weight: 700;
+
+    background: ${({ theme }) => theme.rgb('secondary', 1)};
+    color: ${({ theme }) => theme.rgb('black', 1)};
+
+    box-shadow: ${({ theme }) => {
+      const c1 = theme.rgb('secondary', 0.5, -10);
+      const c2 = theme.rgb('secondary', 0.25, -20);
+      return `
+      0 0 0.1rem 2px ${c1},
+      0 0 0.5rem 1px ${c2}
+      `;
+    }};
+
+    transition: all 225ms;
+
+    &:hover {
+      background: ${({ theme }) => theme.rgb('secondary', 1, theme.isLightMode ? 4 : 8)};
+      transform: translateY(-2px) scale(1.05, 1.02);
+
+      box-shadow: ${({ theme }) => {
+        const c1 = theme.rgb('secondary', 0.5, -10);
+        const c2 = theme.rgb('secondary', 0.25, -20);
+        return `
+        0 0 0.25rem 2px ${c1},
+        0 0 0.75rem 2px ${c2}
+        `;
+      }};
+    }
+  }
+
+  .modal-container {
+    position: fixed;
+    inset: 0;
+    z-index: 100000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: ${({ theme }) => theme.rgb('bg', 0.5)};
+  }
+
+  .modal-content {
+    background: ${({ theme }) => theme.rgb('bg', 1)};
+    width: 100%;
+    max-width: 800px;
+    min-height: 500px;
+    padding: 1rem;
+    border-radius: 1rem;
+    box-shadow: ${({ theme }) =>
+      `0 0 0.25rem 1px inset ${theme.rgb(theme.isLightMode ? 'primary.800' : 'secondary.600', 1)},
+      -0.2rem 0.5rem 1rem 2px ${
+        theme.isLightMode ? theme.rgb('primary.500', 0.5) : theme.rgb('secondary.500', 0.325)
+      }`};
   }
 `;
--- src/components/layout/Markdown.tsx
+++ src/components/layout/Markdown.tsx
@@ -3,11 +3,6 @@ import { FC } from 'react';
 
 const renderer = new Renderer();
 
-renderer.strong = ({ tokens }) => {
-  const text = tokens.map((token) => token.raw).join('');
-  return `<strong class="primary-secondary">${text}</strong>`;
-};
-
 const marked = new Marked({
   renderer,
   gfm: true,
@@ -20,7 +15,6 @@ type MarkdownProps = {
 
 const Markdown: FC<MarkdownProps> = ({ children }) => {
   const mrkdwn = marked.parse(children, { async: false });
-  console.log('Markdown', mrkdwn);
   return <div className='markdown-body' dangerouslySetInnerHTML={{ __html: mrkdwn }} />;
 };
 
