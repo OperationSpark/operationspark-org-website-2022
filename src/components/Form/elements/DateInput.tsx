fcc807b3ef3630571725ef5156c8936d76975754
Peter Barnum <peterbarnum@gmail.com>
2025-04-23 16:22:56 -0600

    fix previously unused date input (now used)

--- src/components/Form/elements/DateInput.tsx
+++ src/components/Form/elements/DateInput.tsx
@@ -1,13 +1,13 @@
-import { getNumsOnly, parseDate } from '../helpers';
-import Input, { TextInputProps } from './Input';
+import { toDayJs } from '@this/src/helpers/time';
+import TextInput, { TextInputProps } from './Input';
 
 const DateInput = ({ onChange, ...props }: TextInputProps) => {
   return (
-    <Input
+    <TextInput
       {...props}
-      type='tel' // use 'tel' to force number pad
-      onChange={(value, isValid) => onChange(parseDate(value), isValid)}
-      validator={(value) => getNumsOnly(value).length >= 8}
+      type='date' // use 'tel' to force number pad
+      onChange={(value, isValid) => onChange(toDayJs(value).format('YYYY-MM-DD'), isValid)}
+      validator={(value) => toDayJs(value).isValid()}
       restoreCursor
     />
   );
