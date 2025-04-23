fc79e0feefbe085e72393d7c15b80bc9ad05fee7
Peter Barnum <peterbarnum@gmail.com>
2025-04-23 16:22:12 -0600

    move form styles to bottom of file

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,25 +2,6 @@ import { MotionProps, motion } from 'framer-motion';
 import { CSSProperties, FormEvent, FormEventHandler, KeyboardEvent, ReactNode } from 'react';
 import styled from 'styled-components';
 
-const FormStyles = styled(motion.form)`
-  display: flex;
-  flex-flow: row wrap;
-  width: 100%;
-  transition: 200ms;
-  .form-error {
-    display: flex;
-    align-items: center;
-    user-select: none;
-    color: ${({ theme }) => (theme.isLightMode ? theme.red[600] : theme.red[300])};
-    p {
-      padding-left: 0.5rem;
-    }
-  }
-  @media print {
-    display: none !important;
-  }
-`;
-
 interface FormProps {
   children?: ReactNode | ReactNode[];
   onSubmit?: (e: FormEvent<HTMLFormElement>) => void;
@@ -50,3 +31,23 @@ const Form = ({ children, onSubmit, style, motionProps, className, onEnter }: Fo
 };
 
 export default Form;
+
+const FormStyles = styled(motion.form)`
+  display: flex;
+  flex-flow: row wrap;
+  width: 100%;
+  transition: 200ms;
+  .form-error {
+    display: flex;
+    align-items: center;
+    user-select: none;
+    color: ${({ theme }) => (theme.isLightMode ? theme.red[600] : theme.red[300])};
+    p {
+      padding-left: 0.5rem;
+    }
+  }
+
+  @media print {
+    display: none !important;
+  }
+`;
