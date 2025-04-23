5d2908e04a92bd9f5768597fd4c989f36c0d678b
Peter Barnum <peterbarnum@gmail.com>
2025-04-23 16:24:52 -0600

    add dev shop form + modal to dev shop view

--- pages/devShop/index.tsx
+++ pages/devShop/index.tsx
@@ -1,5 +1,4 @@
 import { GetStaticProps, NextPage } from 'next';
-import { useState } from 'react';
 import styled from 'styled-components';
 
 import Content from '@this/components/layout/Content';
@@ -9,23 +8,26 @@ import { getStaticAsset } from '@this/pages-api/static/[asset]';
 import NeumorphismListCard from '@this/src/components/Cards/NeumorphismListCard';
 import { BgImg } from '@this/src/components/Elements';
 import GridList from '@this/src/components/Elements/GridList';
+import { Modal, useModal } from '@this/src/components/Elements/Modal';
 import Markdown from '@this/src/components/layout/Markdown';
+import DevShopForm from '@this/src/Forms/Form.DevShop';
 
 const DevShop: NextPage<DevShopType> = ({ title, subtitle, about, skills, contractToHire }) => {
-  const [showModal, setShowModal] = useState(false);
+  const modalCtrl = useModal('Request Info', {
+    blurBackground: true,
+  });
+
   return (
     <Main style={{ paddingTop: 0 }}>
       <DevShopStyles>
-        <button className='req-info-btn' onClick={() => setShowModal(true)}>
+        <button className='req-info-btn btn btn-secondary' onClick={modalCtrl.open}>
           Request Info
         </button>
-        {showModal && (
-          <div className='modal-container'>
-            <div className='modal-content'>
-              <h1 className='dynamic-h2'>TODO: Request Info Modal</h1>
-            </div>
-          </div>
-        )}
+
+        <Modal menu={modalCtrl}>
+          <DevShopForm onSuccess={modalCtrl.close} onCancel={modalCtrl.close} />
+        </Modal>
+
         <BgImg src='/images/display/abstract-space.webp' height='18rem'>
           <Content className='page-header-container'>
             <div className='page-header-content'>
@@ -61,13 +63,20 @@ const DevShop: NextPage<DevShopType> = ({ title, subtitle, about, skills, contra
                 className={`sub-card${grid ? ' center-sub-card' : ''}`}
               >
                 <h3 className='dynamic-h3 text-center fw-900'>{header}</h3>
-                <p
+                <div
                   className={`dynamic-txt sub-card${grid ? ' text-center' : ''}`}
                   key={`skills-performance-1-${i}-${header}`}
                 >
                   <Markdown>{text}</Markdown>
-                </p>
-                {grid && <GridList items={grid} itemStyle={{ flex: '1 1 fit-content' }} />}
+                </div>
+                {grid && (
+                  <GridList
+                    items={grid.map((v, i) => (
+                      <Markdown key={`${v}-${i}`}>{v}</Markdown>
+                    ))}
+                    itemStyle={{ flex: '1 1 fit-content' }}
+                  />
+                )}
               </div>
             ))}
           />
@@ -76,7 +85,7 @@ const DevShop: NextPage<DevShopType> = ({ title, subtitle, about, skills, contra
           <NeumorphismListCard
             title='How Can Operation Spark Help My Company'
             subtitle='Contract to Hire or Our Dev Shop'
-            color='primary'
+            color='green'
             width='1000px'
             style={{ margin: '0 auto' }}
             center
@@ -93,9 +102,17 @@ const DevShop: NextPage<DevShopType> = ({ title, subtitle, about, skills, contra
                     className='checkmark-list center-sub-card dynamic-txt'
                     style={{ margin: '0 auto', width: 'fit-content' }}
                   >
-                    {section.list.map((item, j) => (
-                      <li key={`dev-shop-hire-${i}-${j}`}>{item}</li>
-                    ))}
+                    {section.list.map((item, j) => {
+                      const [title, description] = item.split('--');
+                      return (
+                        <li key={`dev-shop-hire-${i}-${j}`}>
+                          <Markdown>{title}</Markdown>
+                          <div className='checkmark-list-item-desc'>
+                            <Markdown>{description}</Markdown>
+                          </div>
+                        </li>
+                      );
+                    })}
                   </ul>
                 )}
 
@@ -170,71 +187,35 @@ const DevShopStyles = styled.div`
 
   .checkmark-list {
     list-style: '✓ ';
+    p {
+      line-height: 1;
+    }
 
+    li {
+      margin-bottom: 0.5rem;
+    }
     li::marker {
       color: ${({ theme }) =>
         theme.isLightMode ? theme.rgb('green.900', 1) : theme.rgb('green.500', 1)};
     }
   }
+
+  .checkmark-list-item-desc {
+    font-size: 0.8em;
+    padding-left: 0.5rem;
+    margin: 0.125rem 0;
+    margin-left: 0.125rem;
+    line-height: 1;
+    border-left: 2px solid ${({ theme }) => theme.rgb('fg', 0.5)};
+    p {
+      line-height: 1;
+    }
+  }
+
   .req-info-btn {
     position: fixed;
     top: ${({ theme }) => `${theme.navHeight - 20}px`};
     right: 1rem;
     z-index: 1000;
-    padding: 0.5rem 1rem;
-    border-radius: 0.75rem;
-    font-weight: 700;
-
-    background: ${({ theme }) => theme.rgb('secondary', 1)};
-    color: ${({ theme }) => theme.rgb('black', 1)};
-
-    box-shadow: ${({ theme }) => {
-      const c1 = theme.rgb('secondary', 0.5, -10);
-      const c2 = theme.rgb('secondary', 0.25, -20);
-      return `
-      0 0 0.1rem 2px ${c1},
-      0 0 0.5rem 1px ${c2}
-      `;
-    }};
-
-    transition: all 225ms;
-
-    &:hover {
-      background: ${({ theme }) => theme.rgb('secondary', 1, theme.isLightMode ? 4 : 8)};
-      transform: translateY(-2px) scale(1.05, 1.02);
-
-      box-shadow: ${({ theme }) => {
-        const c1 = theme.rgb('secondary', 0.5, -10);
-        const c2 = theme.rgb('secondary', 0.25, -20);
-        return `
-        0 0 0.25rem 2px ${c1},
-        0 0 0.75rem 2px ${c2}
-        `;
-      }};
-    }
-  }
-
-  .modal-container {
-    position: fixed;
-    inset: 0;
-    z-index: 100000;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: ${({ theme }) => theme.rgb('bg', 0.5)};
-  }
-
-  .modal-content {
-    background: ${({ theme }) => theme.rgb('bg', 1)};
-    width: 100%;
-    max-width: 800px;
-    min-height: 500px;
-    padding: 1rem;
-    border-radius: 1rem;
-    box-shadow: ${({ theme }) =>
-      `0 0 0.25rem 1px inset ${theme.rgb(theme.isLightMode ? 'primary.800' : 'secondary.600', 1)},
-      -0.2rem 0.5rem 1rem 2px ${
-        theme.isLightMode ? theme.rgb('primary.500', 0.5) : theme.rgb('secondary.500', 0.325)
-      }`};
   }
 `;
