7aa76803446e5f6f1bee8d28255f5c3208c8a7c3
Peter Barnum <peterbarnum@gmail.com>
2025-04-25 11:34:49 -0600

    move slack, email, and sheets helpers to separate helpers file

--- pages/api/contact/devShop.ts
+++ pages/api/contact/devShop.ts
@@ -1,55 +1,16 @@
-import formData from 'form-data';
-import Mailgun from 'mailgun.js';
-
-import { addRow, createSpreadsheetTab, getSheets } from '@this/api/googleSheets';
-import { DevShopFormInputs } from '@this/data/types/devShop';
-import { connectSlack } from '@this/src/api/slack';
-import { ReqHandler } from '@this/types/request';
 import { capitalize } from 'lodash';
 
-const {
-  DEV_SHOP_INQUIRIES_SHEET_ID,
-  MG_API_KEY = '',
-  MG_DOMAIN = 'mail.operationspark.org',
-  DEV_SHOP_CONTACT_SLACK_CHANNEL_ID,
-} = process.env;
-
-type FormValueType = {
-  name: string;
-  label: string;
-  value: string;
-};
-
-const spreadsheetId = DEV_SHOP_INQUIRIES_SHEET_ID ?? '';
-const formName = 'Dev Shop Inquiries';
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client({
-  username: 'api',
-  key: MG_API_KEY,
-});
-
-const formOutputOrder = ['name', 'company', 'email', 'description'] as const;
-const requiredFields = ['name', 'company', 'email', 'description'] as const;
-
-type FormKey = (typeof formOutputOrder)[number];
-
-const parseSpreadsheetValues = (formValues: FormValueType[]) => {
-  const outputValues = {
-    header: [] as string[],
-    row: [] as string[],
-    valueMap: {} as Record<FormKey, string>,
-  };
-
-  formOutputOrder.forEach((key) => {
-    const { label, value } = formValues.find((formValue) => formValue.name === key) ?? {};
-
-    outputValues.valueMap[key] = value || '';
-    outputValues.header.push(label || '');
-    outputValues.row.push(value || '');
-  });
-
-  return outputValues;
-};
+import { DevShopFormInputs } from '@this/data/types/devShop';
+import {
+  addDevShopContactRowToSheet,
+  checkMissingFields,
+  getDevShopConfirmEmailDetails,
+  parseDevShopSpreadsheetValues,
+  requiredFields,
+  sendDevShopConfirmEmail,
+  sendDevShopInquirySlackMessage,
+} from '@this/src/api/devShopHelpers';
+import { ReqHandler } from '@this/types/request';
 
 const handleContactDevShop: ReqHandler<{}, DevShopFormInputs> = async (req, res) => {
   if (req.method !== 'POST') {
@@ -57,9 +18,9 @@ const handleContactDevShop: ReqHandler<{}, DevShopFormInputs> = async (req, res)
     return;
   }
 
-  const missingFields = requiredFields.filter((field) => !req.body?.[field]);
+  const missingFields = checkMissingFields(req.body);
 
-  if (missingFields.length) {
+  if (missingFields) {
     res.status(400).end(`Missing fields: ${missingFields.join(', ')}`);
     return;
   }
@@ -70,109 +31,34 @@ const handleContactDevShop: ReqHandler<{}, DevShopFormInputs> = async (req, res)
     value: req.body[field],
   }));
 
-  const sheets = getSheets();
-  const { header, row, valueMap } = parseSpreadsheetValues(formValues);
+  const { header, row, valueMap } = parseDevShopSpreadsheetValues(formValues);
 
-  const emailDetails = [
-    {
-      label: 'Name',
-      value: valueMap.name,
-      plainValue: `${valueMap.name} <${valueMap.email}>`,
-    },
-    {
-      label: 'Company',
-      value: valueMap.company,
-      plainValue: valueMap.company,
-    },
-    {
-      label: 'Description',
-      value: valueMap.description,
-      plainValue: valueMap.description,
-    },
-  ];
+  const emailDetails = getDevShopConfirmEmailDetails(valueMap);
 
   try {
-    // Create the tab if it doesn't exist
-    await createSpreadsheetTab({
-      sheets,
-      spreadsheetId,
-      tabName: formName,
-      header,
-    });
-
-    // Add a new row to the spreadsheet
-    await addRow({
-      sheets,
-      spreadsheetId,
-      tabName: formName,
-      values: row,
-    });
+    await addDevShopContactRowToSheet('Dev Shop Inquiries', { header, row });
 
     // Send the response. don't wait for the email or slack message to finish
     res.status(201).end();
 
-    // Finish sending the email and slack message
-    type MgVariables = {
-      title: string;
-      subject: string;
-      body?: string;
-      details: {
-        label: string;
-        value: string;
-      }[];
-    };
+    ////////////////////////////////////////////////////////////
+    // Complete non-blocking tasks after sending the response //
+    ////////////////////////////////////////////////////////////
 
-    const plainTextEmail = `Thank you contacting Operation Spark's Dev Shop.  We will be in touch soon! \n\nDetails:\n${emailDetails
-      .map((detail) => `${detail.label}: ${detail.value}`)
-      .join('\n')}`;
-    const mgVariables: MgVariables = {
-      title: 'Operation Spark | Dev Shop Inquiry Confirmation',
-      subject: 'Dev Shop Inquiry Confirmation',
-      details: emailDetails,
-      body: `Thank you contacting Operation Spark's Dev Shop.  We will be in touch soon!`,
-    };
+    // Send the confirmation email to the user
+    await sendDevShopConfirmEmail(valueMap.email, emailDetails);
 
-    const sendEmail = async () => {
-      try {
-        // Send email
-        await mg.messages.create(MG_DOMAIN, {
-          from: 'Operation Spark <noreply@operationspark.org>',
-          to: valueMap.email,
-          subject: mgVariables.subject,
-          template: 'dev-shop-contact-confirmation',
-          text: plainTextEmail,
-          'h:X-Mailgun-Variables': JSON.stringify(mgVariables),
-        });
-      } catch (error) {
-        // Don't fail for email
-        console.error('Error sending email:', error);
-      }
-    };
-    await sendEmail();
-
-    try {
-      const slackMessage = `\
-      \n*${valueMap.name}* has contacted the Dev Shop!\
-      \n*Email:* ${valueMap.email}\
-      \n*Company:* ${valueMap.company}\
-      \n*Description:*\n> ${valueMap.description.split('\n').join('\n> ')}`;
-
-      const slackClient = connectSlack();
-      await slackClient.chat.postMessage({
-        channel: DEV_SHOP_CONTACT_SLACK_CHANNEL_ID,
-        text: slackMessage,
-        blocks: [{ type: 'section', text: { type: 'mrkdwn', text: slackMessage } }],
-      });
-    } catch (error) {
-      // Don't fail for slack
-      console.error('Error sending Slack message:', error);
-    }
+    await sendDevShopInquirySlackMessage(valueMap);
 
     // Status sent after details added to spreadsheet above
+    return;
   } catch (error) {
     console.error('Error handling contact form:', error);
     res.status(500).end();
+    return;
   }
+
+  res.status(500).end();
 };
 
 export default handleContactDevShop;
---
+++ src/api/devShopHelpers.ts
@@ -0,0 +1,174 @@
+import { DevShopFormInputs } from '@this/data/types/devShop';
+import formData from 'form-data';
+import Mailgun from 'mailgun.js';
+import { addRow, createSpreadsheetTab, getSheets } from './googleSheets';
+import { connectSlack } from './slack';
+
+const {
+  DEV_SHOP_INQUIRIES_SHEET_ID,
+  MG_API_KEY = '',
+  MG_DOMAIN = 'mail.operationspark.org',
+  DEV_SHOP_CONTACT_SLACK_CHANNEL_ID,
+} = process.env;
+
+export const formOutputOrder = ['name', 'company', 'email', 'description'] as const;
+export const requiredFields = ['name', 'company', 'email', 'description'] as const;
+
+type FormKey = (typeof formOutputOrder)[number];
+
+type FormValueType = {
+  name: string;
+  label: string;
+  value: string;
+};
+type emailDetail = {
+  label: string;
+  value: string;
+  plainValue: string;
+};
+// Finish sending the email and slack message
+type MgVariables = {
+  title: string;
+  subject: string;
+  body?: string;
+  details: {
+    label: string;
+    value: string;
+  }[];
+};
+
+type ParsedSheetValues = {
+  header: string[];
+  row: string[];
+  valueMap: Record<FormKey, string>;
+};
+
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+  username: 'api',
+  key: MG_API_KEY,
+});
+
+export const checkMissingFields = (values: DevShopFormInputs) => {
+  if (!values) {
+    return requiredFields;
+  }
+
+  const missingFields = requiredFields.filter((field) => !values[field]);
+
+  if (!missingFields.length) {
+    return null;
+  }
+
+  if (missingFields.length) {
+    return missingFields;
+  }
+};
+
+export const parseDevShopSpreadsheetValues = (formValues: FormValueType[]): ParsedSheetValues => {
+  const outputValues = {
+    header: [] as string[],
+    row: [] as string[],
+    valueMap: {} as Record<FormKey, string>,
+  };
+
+  formOutputOrder.forEach((key) => {
+    const { label, value } = formValues.find((formValue) => formValue.name === key) ?? {};
+
+    outputValues.valueMap[key] = value || '';
+    outputValues.header.push(label || '');
+    outputValues.row.push(value || '');
+  });
+
+  return outputValues;
+};
+
+export const getDevShopConfirmEmailDetails = (values: DevShopFormInputs): emailDetail[] => {
+  return [
+    {
+      label: 'Name',
+      value: values.name,
+      plainValue: `${values.name} <${values.email}>`,
+    },
+    {
+      label: 'Company',
+      value: values.company,
+      plainValue: values.company,
+    },
+    {
+      label: 'Description',
+      value: values.description,
+      plainValue: values.description,
+    },
+  ];
+};
+
+export const sendDevShopConfirmEmail = async (toEmail: string, emailDetails: emailDetail[]) => {
+  const plainTextEmail = `Thank you contacting Operation Spark's Dev Shop.  We will be in touch soon! \n\nDetails:\n${emailDetails
+    .map((detail) => `${detail.label}: ${detail.value}`)
+    .join('\n')}`;
+  const mgVariables: MgVariables = {
+    title: 'Operation Spark | Dev Shop Inquiry Confirmation',
+    subject: 'Dev Shop Inquiry Confirmation',
+    details: emailDetails,
+    body: `Thank you contacting Operation Spark's Dev Shop.  We will be in touch soon!`,
+  };
+  try {
+    // Send email
+    await mg.messages.create(MG_DOMAIN, {
+      from: 'Operation Spark <noreply@operationspark.org>',
+      to: toEmail,
+      subject: mgVariables.subject,
+      template: 'dev-shop-contact-confirmation',
+      text: plainTextEmail,
+      'h:X-Mailgun-Variables': JSON.stringify(mgVariables),
+    });
+  } catch (error) {
+    // Don't fail for email
+    console.error('Error sending email:', error);
+  }
+};
+
+export const addDevShopContactRowToSheet = async (
+  formName: string,
+  parsedSheetValues: Omit<ParsedSheetValues, 'valueMap'>,
+) => {
+  const spreadsheetId = DEV_SHOP_INQUIRIES_SHEET_ID;
+  const { header, row } = parsedSheetValues;
+  const sheets = getSheets();
+  // Create the tab if it doesn't exist
+  await createSpreadsheetTab({
+    sheets,
+    spreadsheetId,
+    tabName: formName,
+    header,
+  });
+
+  // Add a new row to the spreadsheet
+  await addRow({
+    sheets,
+    spreadsheetId,
+    tabName: formName,
+    values: row,
+  });
+};
+
+export const sendDevShopInquirySlackMessage = async (valueMap: DevShopFormInputs) => {
+  const slackMessage = `\
+  \n*${valueMap.name}* has contacted the Dev Shop!\
+  \n*Email:* ${valueMap.email}\
+  \n*Company:* ${valueMap.company}\
+  \n*Description:*\n> ${valueMap.description.split('\n').join('\n> ')}`;
+
+  try {
+    const slackClient = connectSlack();
+    await slackClient.chat.postMessage({
+      channel: DEV_SHOP_CONTACT_SLACK_CHANNEL_ID,
+      text: slackMessage,
+      blocks: [{ type: 'section', text: { type: 'mrkdwn', text: slackMessage } }],
+    });
+  } catch (error) {
+    // Don't fail for slack
+    console.error('Error sending Slack message:', error);
+  }
+};
